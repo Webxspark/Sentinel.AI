0fa0adbf76f7e31f6dfd9da272782cb901b18e42
Annamalai Prabu <annamalaiprabu9@gmail.com>
2023-06-11 08:14:21 +0530

    Server Files updated

---
+++ server/env.py
@@ -0,0 +1,8 @@
+def getPrivateKey():
+   return 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+OPENAI_API_KEY = "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+TWILIO_AUTH_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ASSEMBLY_AI_AUTH_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+CONTRACT_ADDRESS = "0x0d284xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
